db02986088fe0c7dcb60e7a6058b638a136806ce
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-24 00:03:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+candy = ["twix", "m&ms", "gummy bears", "skittles", "hi-chews"]
+
+candy_inventory = []
+
+#candy bad application
+print("Treat or treat. Choose your own addition")
+#ask the user for what candy they'd like to add to their bag
+for i in candy_inventory:
+    print(i) 
+print()
+
+#input validation
+
+selection = input("Pick a candy:  ").lower()
+
+while selection not in candy_bag:.lower()
+
+selection = input("Pick another candy:  ").lower()
+
+
+#display all their options from the candy_inventorhy
+#set candy_bag length to be 3. max
+#every time the user adds candy to their bag, display their candy bag items
+#once the user reaches their max of 3, do a trick or treat greeting
